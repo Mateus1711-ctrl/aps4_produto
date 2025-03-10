9f838cd28c290d6a79046bbc76fbef2d205535a7
Mateus Porto <mateusppp96@gmail.com>
2025-03-10 11:45:01 -0300

    inicio2

---
+++ aps_produto/aps_produto/src/test/java/aps/produto/aps_produto/ApsProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package aps.produto.aps_produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
